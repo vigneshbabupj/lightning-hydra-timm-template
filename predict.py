d84d10d1cf2bd8c99e872225590d77b51a6c5b24
Vignesh Babu P J <vigneshbabupj@gmail.com>
2022-09-09 11:12:19 +0000

    Update to Ver 2

--- predict.py
+++ predict.py
@@ -7,27 +7,25 @@ root = pyrootutils.setup_root(
     dotenv=True,
 )
 
-
+import json
 import hydra
+import timm
+import torch
+import numpy as np
+from PIL import Image
+from typing import Any
 from omegaconf import DictConfig
 from hydra import compose, initialize
-
-from typing import Any
 from cog import BasePredictor, Input, Path
-
-import json
-import torch
-import timm
-import numpy as np
 from torchvision.transforms import transforms
 from pytorch_lightning import LightningModule
-from PIL import Image
+
 
 
 class Predictor(BasePredictor):
 
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
+        """Load the model into memory to make running multiple predictions efficient."""
         # self.model = timm.create_model('efficientnet_b3a', pretrained=True)
         # self.model = model
         
@@ -39,11 +37,24 @@ class Predictor(BasePredictor):
         # model.load_from_checkpoint(checkpoint_path = "/workspace/Gitpod/lightning-hydra-template/logs/train/runs/2022-09-04_12-10-26/checkpoints/epoch_000.ckpt")
         self.model.eval()
         self.transform = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.4915, 0.4823, .4468), (0.2470, 0.2435, 0.2616))]
+            [
+                transforms.ToTensor(), 
+                transforms.Normalize((0.4915, 0.4823, .4468), (0.2470, 0.2435, 0.2616))
+            ]
         )
 
-        self.labels = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        self.labels = (
+            "plane", 
+            "car", 
+            "bird", 
+            "cat",
+            "deer", 
+            "dog", 
+            "frog", 
+            "horse", 
+            "ship", 
+            "truck"
+            )
 
     # Define the arguments and types the model takes as input
     def predict(self, image: Path = Input(description="Image to classify")) -> Any:
